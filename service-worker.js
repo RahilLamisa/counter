fff24ea3ec3195ec05c86ed33e4926f43dd4e34b
RahilLamisa <tasnimrahil2003002@gmail.com>
2025-03-04 01:19:37 +0600

    modified

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "tasbih-counter-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch((error) => console.error("Service Worker Registration Failed:", error));
+}
