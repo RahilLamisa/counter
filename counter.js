5606f476af290e7bc9805ec46a3ca4cbf77df01f
RahilLamisa <tasnimrahil2003002@gmail.com>
2025-03-03 21:36:57 +0600

    final

--- counter.js
+++ counter.js
@@ -1,1 +1,97 @@
-// alert('Welcome 🤗')
+alert('Welcome 🤗')
+document.getElementById('subhanallah').addEventListener('click',function(){
+    // console.log('okay');
+    let count1 = document.getElementById('count1').innerText;
+    let convertedCount1 = parseInt(count1);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount1 + 1;
+    document.getElementById('count1').innerText = sum;
+})
+document.getElementById('ref1').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count1').innerText='0';
+})
+
+
+document.getElementById('alhamdulillah').addEventListener('click',function(){
+    // console.log('okay');
+    let count2 = document.getElementById('count2').innerText;
+    let convertedCount2 = parseInt(count2);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount2 + 1;
+    document.getElementById('count2').innerText = sum;
+})
+document.getElementById('ref2').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count2').innerText='0';
+})
+
+
+document.getElementById('allahuakbar').addEventListener('click',function(){
+    // console.log('okay');
+    let count3 = document.getElementById('count3').innerText;
+    let convertedCount3 = parseInt(count3);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount3 + 1;
+    document.getElementById('count3').innerText = sum;
+})
+document.getElementById('ref3').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count3').innerText='0';
+})
+
+
+document.getElementById('lailahaillallah').addEventListener('click',function(){
+    // console.log('okay');
+    let count4 = document.getElementById('count4').innerText;
+    let convertedCount4 = parseInt(count4);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount4 + 1;
+    document.getElementById('count4').innerText = sum;
+})
+document.getElementById('ref4').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count4').innerText='0';
+})
+
+
+document.getElementById('sallallah').addEventListener('click',function(){
+    // console.log('okay');
+    let count5 = document.getElementById('count5').innerText;
+    let convertedCount5 = parseInt(count5);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount5 + 1;
+    document.getElementById('count5').innerText = sum;
+})
+document.getElementById('ref5').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count5').innerText='0';
+})
+
+
+document.getElementById('ajim').addEventListener('click',function(){
+    // console.log('okay');
+    let count6 = document.getElementById('count6').innerText;
+    let convertedCount6 = parseInt(count6);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount6 + 1;
+    document.getElementById('count6').innerText = sum;
+})
+document.getElementById('ref6').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count6').innerText='0';
+})
+
+
+document.getElementById('others').addEventListener('click',function(){
+    // console.log('okay');
+    let count7 = document.getElementById('count7').innerText;
+    let convertedCount7 = parseInt(count7);
+    // console.log(typeof convertedCount1);
+    let sum = convertedCount7 + 1;
+    document.getElementById('count7').innerText = sum;
+})
+document.getElementById('ref7').addEventListener('click',function(){
+    // console.log('okay');
+    document.getElementById('count7').innerText='0';
+})
